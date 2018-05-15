2a09700f5fce370df07f786eccfbcfb94ca92886
Rajendra <rajendraprasadmalla90@gmail.com>
2018-05-15 18:44:03 +0530

    ng-style

--- src/app/component/dishes/dishes.component.ts
+++ src/app/component/dishes/dishes.component.ts
@@ -8,34 +8,43 @@ import { Items } from '../../data/storage';
 })
 export class DishesComponent {
 
-recepes: Items[];
+  recepes: Items[];
 
-recepe_in_progress: Items;
+  recepe_in_progress: Items;
 
-colorChange: boolean;
+  colorChange: any;
+  i: any;
+  font_size: any;
+  constructor() {
+    this.i = 1;
+    this.font_size = { 'font-size': '100%' };
+    this.colorChange = { addclass: false };
+    this.recepe_in_progress = Items.createBlank();
 
-constructor() {
- this.colorChange = false;
-  this.recepe_in_progress = Items.createBlank();
-  this.recepes = [
-    new Items('samosa', 'very spicy you can choose how much spicy you want', 3, 40, null, null, null),
-    new Items('parota', 'very tasty, and you can choose type', 2, 34, null , null, null),
-    new Items('bajji', 'very spicy, you can choose how much spicy you want', 5, 30,  null, null, null)
-  ];
- }
+    this.recepes = [
+      new Items('samosa', 'very spicy you can choose how much spicy you want', 3, 40, null, null, null),
+      new Items('parota', 'very tasty, and you can choose type', 2, 34, null, null, null),
+      new Items('bajji', 'very spicy, you can choose how much spicy you want', 5, 30, null, null, null)
+    ];
+  }
 
- public addRecepe() {
-   console.log(JSON.stringify(this.recepe_in_progress));
-   this.recepes.unshift(this.recepe_in_progress);
-   this.recepe_in_progress = Items.createBlank();
- }
+  public addRecepe() {
+    console.log(JSON.stringify(this.recepe_in_progress));
+    this.recepes.unshift(this.recepe_in_progress);
+    this.recepe_in_progress = Items.createBlank();
+  }
 
- public getRecepe(recepe) {
-   console.log('recepe been clicked');
-   alert(JSON.stringify(recepe, null, 2));
- }
+  public getRecepe(recepe) {
+    console.log('recepe been clicked');
+    alert(JSON.stringify(recepe, null, 2));
+  }
 
- public changeClass() {
-   this.colorChange = !this.colorChange;
- }
+  public changeClass() {
+    const newva = !this.colorChange['addclass'];
+    this.colorChange = { 'addclass': newva };
+  }
+
+  public changefont() {
+    if (this.font_size['font-size'] === '100%') { this.font_size['font-size'] = '150%'; } else { this.font_size['font-size'] = '190%'; }
+  }
 }
